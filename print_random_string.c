135d52fcf067b8682452558dc9149dc862a1492a
Felix Hardjana <tur84384@temple.edu>
2025-08-29 21:50:34 -0400

    created random.c and random.h file for print_random_string

--- print_random_string.c
+++ print_random_string.c
@@ -3,8 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "random.h"
 
-
+void rand_string(char *s, size_t size); 
 
 // random string generator wrapper function
 char* rand_string_alloc(size_t size)
@@ -22,7 +23,7 @@ int main()
 
 {
     char *str;
-    size_t size =   ;  //specify a value for size of word
+    size_t size =  5;  //specify a value for size of word
 
 	/* seed the randomizer */
 	srand( (unsigned)time(NULL) );
---
+++ random.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "random.h"
+
+void rand_string(char *s, size_t size){
+
+    for(size_t i = 0; i < size; i++){
+        s[i] = 'a' + rand()%26; //randomize all the letter selection
+    }
+    s[size] = '\0'; // the last space in the size is always null terminator in string
+}
